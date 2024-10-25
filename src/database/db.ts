407d7fe8f2e842844372c09d6f345473d638c790
lamula21 <jvaldiv8@montgomerycollege.edu>
2024-10-24 23:05:38 -0500

    setup biome.js linter

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,18 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
 // globalThis - a constant that persists across multipe server restarts
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const db =
-  globalForPrisma.prisma || new PrismaClient()
+export const db = globalForPrisma.prisma || new PrismaClient()
 
-// In development or testing mode, we want to reuse the same connection 
+// In development or testing mode, we want to reuse the same connection
 // after server restart, so we store it in a global variable (globalForPrisma).
 // After server restarts, we will reuse the global connection (line 5).
-// In production mode, globalForPrisma will always be `undefined` because 
+// In production mode, globalForPrisma will always be `undefined` because
 // in this line it's never set.
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
 
 // The database url strings can be uploaded dynamically through
 // the env variables for production and development environments
